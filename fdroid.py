0e0a380c3419994f6f94314f993eead657546ed7
Amirhossein Atashkar <ahatashkar@gmail.com>
2020-09-09 11:25:32 +0430

    find source code link

--- fdroid.py
+++ fdroid.py
@@ -7,6 +7,24 @@ class FDroid(scrapy.Spider):
 
     def parse(self, response):
         for app in response.css('a.package-header'):
-            yield {
-                'link': "https://f-droid.org/" + app.xpath('@href').get(),
-            }
+            # link = "https://f-droid.org/" + app.xpath('@href').get()
+            link = response.urljoin(app.css('::attr(href)').get())
+
+            # yield {
+            #     'link': link,
+            # }
+
+            yield scrapy.Request(link, callback=self.parse_page)
+
+    def parse_page(self, response):
+        for link in response.css('.package-link'):
+            if link.css('a::text').get() == 'Source Code':
+                source_code = response.urljoin(link.css('a::attr(href)').get())
+
+
+        # source_code = response.urljoin(response.css('.package-link a::attr(href)')[4].get())
+
+        yield {
+            'link': response.request.url,
+            'source': source_code
+        }

cb8ac1a3b6f78dd54270bfe5c1952f4330847e7b
Amirhossein Atashkar <ahatashkar@gmail.com>
2020-09-09 16:49:41 +0430

    find all github sourced apps

--- fdroid.py
+++ fdroid.py
@@ -1,4 +1,5 @@
 import scrapy
+import os
 
 
 class FDroid(scrapy.Spider):
@@ -7,15 +8,15 @@ class FDroid(scrapy.Spider):
 
     def parse(self, response):
 
-        app = response.css('a.package-header')
-        link = response.urljoin(app.css('::attr(href)').get())
-        yield scrapy.Request(link, callback=self.parse_page)
+        # app = response.css('a.package-header')
+        # link = response.urljoin(app.css('::attr(href)').get())
+        # yield scrapy.Request(link, callback=self.parse_page)
 
-        # for app in response.css('a.package-header'):
-        #     # link = "https://f-droid.org/" + app.xpath('@href').get()
-        #     link = response.urljoin(app.css('::attr(href)').get())
-        #
-        #     yield scrapy.Request(link, callback=self.parse_page)
+        for app in response.css('a.package-header'):
+            # link = "https://f-droid.org/" + app.xpath('@href').get()
+            link = response.urljoin(app.css('::attr(href)').get())
+
+            yield scrapy.Request(link, callback=self.parse_page)
 
     def parse_page(self, response):
         source_code = ""
@@ -23,23 +24,19 @@ class FDroid(scrapy.Spider):
             if link.css('a::text').get() == 'Source Code':
                 source_code = link.css('a::attr(href)').get()
 
-        yield {
-            'link': response.request.url,
-            'source': source_code
-        }
-
-        item = ZipfilesItem()
-        item['file_urls'] = [response.urljoin(source_code + "/archive/master.zip")]
-        yield item
-
-        # yield scrapy.Request(response.urljoin(source_code + "/archive/master.zip"), callback=self.save_project)
+        if "github" in source_code:
+            yield {
+                'link': response.request.url,
+                'source': source_code
+            }
+            yield scrapy.Request(response.urljoin(source_code + "/archive/master.zip"), callback=self.save_project)
 
     def save_project(self, response):
-        path = response.url
-        with open(path, 'wb') as f:
+        if not os.path.exists('Projects'):
+            os.mkdir('Projects')
+
+        name = response.url.split('/')[-3]
+        with open("Projects/" + name, 'wb') as f:
             f.write(response.body)
+            f.close()
 
-
-class ZipfilesItem(scrapy.Item):
-    file_urls = scrapy.Field()
-    files = scrapy.Field

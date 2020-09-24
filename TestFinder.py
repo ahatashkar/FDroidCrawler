fc0ee9f7507dc6da2c6d5ef0a7585488e1d8afc2
Amirhossein Atashkar <ahatashkar@gmail.com>
2020-09-24 10:27:23 +0330

    add lines of codes and tests

--- TestFinder.py
+++ TestFinder.py
@@ -10,9 +10,12 @@ total_test = 0
 total_junit = 0
 total_espresso = 0
 
+lines_code = 0
+lines_test = 0
+
 
 def analyze(dir):
-    global has_junit, has_espresso, count_espresso, count_junit
+    global has_junit, has_espresso, count_espresso, count_junit, lines_test, lines_code
 
     for each in os.listdir(dir):
         current = os.path.join(dir, each)
@@ -24,9 +27,26 @@ def analyze(dir):
                 datafile = open(current)
                 lines = datafile.readlines()
 
+                temp_lines = []
+                for line in lines:
+                    if line.lstrip().startswith("package") or \
+                            line.startswith("\n") or \
+                            line.lstrip().startswith("import") or \
+                            line.lstrip().startswith('//') or \
+                            line.lstrip().startswith("/*") or \
+                            line.lstrip().startswith("*") or \
+                            line.lstrip().startswith("*/"):
+                        continue
+                    else:
+                        temp_lines.append(line)
+
+                has_test_flag = False
                 for line in lines:
                     if 'org.junit.Test' in line:
+                        has_test_flag = True
+                        lines_test = lines_test + temp_lines.__len__()
                         espresso_flag = False
+
                         for temp_line in lines:
                             if 'androidx.test.espresso' in temp_line \
                                     or 'android.support.test.espresso' in temp_line:
@@ -46,23 +66,8 @@ def analyze(dir):
                                 if '@Test' in temp:
                                     count_junit = count_junit + 1
 
-
-                    # if 'androidx.test.espresso' in line \
-                    #         or 'android.support.test.espresso' in line:
-                    #     has_espresso = 1
-                    #
-                    #     for temp_line in lines:
-                    #         if '@Test' in temp_line:
-                    #             count_espresso = count_espresso + 1
-                    #
-                    #     break
-                    #
-                    # elif 'org.junit.Test' in line:
-                    #     has_junit = 1
-                    #
-                    #     for temp_line in lines:
-                    #         if '@Test' in temp_line:
-                    #             count_junit = count_junit + 1
+                if not has_test_flag:
+                    lines_code = lines_code + temp_lines.__len__()
 
 
 root = os.getcwd() + '/Projects/Unzip'
@@ -77,6 +82,8 @@ for directory in os.listdir(root):
         has_espresso = 0
         count_espresso = 0
         count_junit = 0
+        lines_code = 0
+        lines_test = 0
 
         analyze('.')
         if has_junit > 0 or has_espresso > 0:
@@ -86,7 +93,13 @@ for directory in os.listdir(root):
         if has_espresso > 0:
             total_espresso = total_espresso + 1
 
-        print(directory + ' : ' + str(has_junit) + ' , ' + str(has_espresso) + ' , ' + str(count_junit) + ' , ' + str(count_espresso))
+        print(directory + ' : ' +
+              str(has_junit) + ' , ' +
+              str(has_espresso) + ' , ' +
+              str(count_junit) + ' , ' +
+              str(count_espresso) + ' , ' +
+              str(lines_code) + ' , ' +
+              str(lines_test))
 
 print('----------------------------')
 print('total projects: ' + str(total))
